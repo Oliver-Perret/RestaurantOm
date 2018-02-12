20a2c7051b95bbec9af29f72459334713aa0f7e3
OliverPerret <oliver.perret2@gmail.com>
2018-02-12 19:26:41 +0100

    Ajout fichier javascript

---
+++ source/javascripts/popover.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+        $(".img-popover").popover({
+          html: true,
+          trigger: "hover",
+          placement: "left"
+
+        });
+        // Your popover Bootstrap javascript code here
+
+      })
